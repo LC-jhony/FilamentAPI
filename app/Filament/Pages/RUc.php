8a737188ce312e5228e000d10a5512eeedd475b2
LC-jhony <jhonyapm94@gmail.com>
2025-12-09 14:56:25 -0500

    updated

---
+++ app/Filament/Pages/RUc.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class RUc extends Page
+{
+    protected string $view = 'filament.pages.r-uc';
+}
--- resources/views/filament/pages/dni-DNI.blade.php
+++ resources/views/filament/pages/dni-DNI.blade.php
@@ -7,8 +7,8 @@
 @if ($resultado)
     <div class="fi-section rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
         style="margin-top: 1rem;">
-        <div class="fi-section-content p-4">
-            <pre class="text-sm text-gray-600 dark:text-gray-400"
+        <div class="fi-section-content p-4" style="padding: 8px">
+            <pre class="text-sm text-gray-600 dark:text-gray-400  "
                 style="font-family: ui-monospace, monospace; white-space: pre-wrap; margin: 0;">{{ json_encode($resultado, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
         </div>
     </div>
